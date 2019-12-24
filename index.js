bb55d337b1d280356464b538b4daba8723fe7f80
aungchanoo <adsltheprescence@gmail.com>
2019-12-24 11:47:48 +0630

    aaa

---
+++ index.js
@@ -0,0 +1,98 @@
+'use strict';
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  requestify = require('requestify'),
+  app = express().use(bodyParser.json()); // creates express http server
+
+  const pageaccesstoken = 'EAAKGyWXj6KABAHt8sUrcubc2QZCoGOVIZCZBik2EcCgX8t03JJrK0WekgQLlEBzZA9ZCHDkvQ7MxbMSzshoFtym0CX7nDYFVQKt6ZAwIILPh7ZBwWX94pupeOh1ZCREUmf7zBwGx8RsixwZBoBkh4LnG0zECHZCPzYSlppXskBkQlDc6U0OtsreW7dh9yhxtORsYMZD'
+
+  requestify.post(`https://graph.facebook.com/v2.6/me/messenger_profile?access_token=${pageaccesstoken}`, 
+  {
+    "get_started": {
+      "payload": "Hi"
+    },
+    "greeting": [
+      {
+        "locale":"default",
+        "text":"Hello {{user_first_name}}!" 
+      }, {
+        "locale":"en_US",
+        "text":"Timeless apparel for the masses."
+      }
+    ]
+  }
+).then( response => {
+  console.log(response)
+}).fail( error => {
+  console.log(error)
+})
+
+  // Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "aungchanoo"
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+        if(webhook_event.message){
+          var userInput = webhook_event.message.text
+        }
+        if(webhook_event.postback){
+          var userButton = webhook_event.postback.payload
+        }
+
+        
+
+          
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
