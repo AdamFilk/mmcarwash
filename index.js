aac1e00ff0e42a28d3ff41d25080a9267c319df0
aungchanoo <adsltheprescence@gmail.com>
2020-03-02 20:44:25 +0630

    Update index.js

--- index.js
+++ index.js
@@ -96,40 +96,45 @@ app.post('/webhook', (req, res) => {
               "text":"Hi! Welcome from MM Car Wash 😄😄😄"
             }
           };
-          let genericMessage = {
+          let quickReply = {
             "recipient":{
               "id": webhook_event.sender.id
             },
+            "messaging_type": "RESPONSE",
             "message":{
-              "attachment":{
-                "type":"template",
-                "payload":{
-                  "template_type":"generic",
-                  "elements":[
-                    {
-                      //star book
-                  
-                    "title":"🚲MM Carwash🚲",
-                    "subtitle":"Choose one",
-                    "image_url":"https://i.pinimg.com/236x/84/77/8e/84778ecda6db5a14c94883948af73262.jpg",
-                    "buttons":[
-                      {
-                        "type":"postback",
-                        "title":"Start Booking",
-                        "payload":"bcw"
-                      },
-                
-                      {
-                        "type":"postback",
-                        "title":"Check Prices",
-                        "payload":"prices"
-                      }
-                    ]
-  
-                  }
-                ]
+              "text": "Please Select One:",
+              "quick_replies":[
+                {
+                  "content_type":"text",
+                  "title":"Book",
+                  "payload":"book",
+                  "image_url":"https://cdn0.iconfinder.com/data/icons/business-butterscotch-vol-2/512/Schedule-512.png"
+                },
+                {
+                  "content_type":"text",
+                  "title":"Prices",
+                  "payload":"prices",
+                  "image_url":"https://cdn3.vectorstock.com/i/1000x1000/89/87/bag-money-icon-blue-vector-19838987.jpg"
+                },
+                {
+                  "content_type":"text",
+                  "title":"View Appointment",
+                  "payload":"view_ap",
+                  "image_url":"https://cdn2.vectorstock.com/i/1000x1000/49/66/automatic-car-wash-icon-vector-11514966.jpg"
+                },
+                {
+                  "content_type":"text",
+                  "title":"About",
+                  "payload":"about",
+                  "image_url":"https://i.pinimg.com/originals/65/8f/c9/658fc9a628b7fb1996966cb07eccc33e.jpg"
+                },
+                {
+                  "content_type":"text",
+                  "title":"Contact",
+                  "payload":"contact",
+                  "image_url":"https://clipartart.com/images/contact-logo-clipart-6.jpg"
                 }
-              }
+              ]
   
             }
           }
@@ -141,7 +146,7 @@ app.post('/webhook', (req, res) => {
           console.log(error)
         })
         requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, 
-        genericMessage
+        quickReply
         ).then(response=>{
           console.log(response)
         }).fail(error=> {
