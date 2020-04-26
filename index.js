dcb4742bcbcfd7677df55297e03a3b11b7d75c4f
aungchanoo <adsltheprescence@gmail.com>
2020-04-26 14:39:43 +0630

    a

--- index.js
+++ index.js
@@ -419,6 +419,7 @@ app.post('/webhook', (req, res) => {
                         "type":"web_url",
                         "url":"https://mmcarwash.herokuapp.com/carwash/"+userInput+"/"+udetails.name+"/"+senderID,
                         "title":"Fill the Form",
+                        "messenger_extensions":true,
                         "webview_height_ratio": "full",
                       },
                       
